44961cfd54901b466fd0bb8e5226f1ab8b35ef73
Eriko Abe <e.abe1199924@gmail.com>
2025-12-16 15:53:19 +0900

    Next.js、Express.jsの開発環境構築

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
