a99fb6e3d30ff2c9183fb6a97e52a51c76490ca2
Eriko Abe <e.abe1199924@gmail.com>
2025-12-17 12:09:27 +0900

    残高照会API呼び出し

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import router from './balance/balance.routes';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const PORT = 4000;
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+app.use('/api', router);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
