298ac5051ae59f24c191ec1b6dbef0f15088f4d3
f-tsurumaki <f-tsurumaki@outlook.jp>
2025-12-17 11:00:21 +0900

    server.tsの作成

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,10 @@
 import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = express();
-const PORT = 4000;
 
+// ルーティング
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+export default app;
