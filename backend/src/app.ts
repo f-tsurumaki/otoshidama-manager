a92179416f7d84ebc5d70977405ea171bd3a0f75
f-tsurumaki <f-tsurumaki@outlook.jp>
2025-12-18 09:42:57 +0900

    app.tsにtransactionをインポート

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,22 @@
 import express, { Request, Response, NextFunction } from "express";
 
+import cors from "cors";
 // accounts 配下のルーティングを使うために router を import
 import accountsRouter from "./accounts/account.routes";
 import router from "./balance/balance.routes";
+import transactionRouter from "./transactions/transaction.routes";
+
 
 const app = express();
 
 // JSON リクエストボディを扱えるようにする middleware
+app.use(cors());
 app.use(express.json());
 
 // 口座一覧など accounts 関連 API を /accounts 配下で使えるようにする
 app.use("/accounts", accountsRouter);
+
+app.use("/transactions", transactionRouter); //取引明細関連
 
 // ルーティング
 app.get("/", (req: Request, res: Response) => {
