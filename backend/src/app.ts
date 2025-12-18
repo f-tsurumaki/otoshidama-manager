9ca3c45294abfcd090ec841ab9c7e4b2d611b24b
f-tsurumaki <f-tsurumaki@outlook.jp>
2025-12-19 00:36:45 +0900

    app.tsのルーティング整理

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,6 @@ import cors from "cors";
 // accounts 配下のルーティングを使うために router を import
 import balanceRouter from "./balance/balance.routes";
 import accountsRouter from "./accounts/account.routes";
-
-import router from "./balance/balance.routes";
 import transferRouter from "./transfers/transfer.routes";
 import transactionRouter from "./transactions/transaction.routes";
 
@@ -17,19 +15,13 @@ app.use(express.json());
 // 口座一覧など accounts 関連 API を /accounts 配下で使えるようにする
 app.use("/accounts", accountsRouter);
 app.use("/accounts/balance", balanceRouter);
-
+app.use("/api/transfers", transferRouter);
 app.use("/transactions", transactionRouter); //取引明細関連
 
 // ルーティング
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
-
-// balance.routes.ts
-app.use("/api", router);
-
-//transfer.routes.ts
-app.use("/api/transfers", transferRouter);
 
 // ←ここからエラーハンドラーを追加
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
