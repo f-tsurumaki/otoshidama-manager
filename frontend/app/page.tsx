8796ff3f830f5d29016843f85433a5b894a198c9
f-tsurumaki <f-tsurumaki@outlook.jp>
2025-12-19 10:54:58 +0900

    余白の調整と見出しの文字サイズ変更

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,16 +7,16 @@ import SubAccountBalance from "@/components/balance/SpAccountBalance";
 export default function Page() {
   return (
     // 画面全体のレイアウトを担当
-    <main className="min-h-screen bg-zinc-50 flex justify-center">
+    <main className="min-h-screen flex justify-center mt-12">
       <div className="w-full max-w-6xl p-6">
         <section className="mb-6">
-          <h2 className="text-xl font-bold">＜銀行残高＞</h2>
+          <h2 className="text-2xl font-bold">＜銀行残高＞</h2>
           <li className="flex items-center justify-center border rounded-md p-4 mb-2 bg-white">
             <BalanceCard />
           </li>
         </section>
         <section className="mb-6">
-          <h2 className="text-xl font-bold">＜振り分け明細＞</h2>
+          <h2 className="text-2xl font-bold">＜振り分け明細＞</h2>
           <ul>
             <li>
               <SubAccountName index={1} />
@@ -34,7 +34,7 @@ export default function Page() {
         </section>
 
         <section className="mb-6">
-          <h2 className="text-xl font-bold">＜振り分け登録＞</h2>
+          <h2 className="text-2xl font-bold">＜振り分け登録＞</h2>
 
           <div className="flex justify-between gap-4">
             <div className="flex-1 border rounded p-4 bg-purple-400 text-center text-white">
@@ -50,7 +50,7 @@ export default function Page() {
         </section>
 
         <section className="mb-6">
-          <h2 className="text-xl font-bold">＜入出金明細＞</h2>
+          <h2 className="text-2xl font-bold">＜入出金明細＞</h2>
           <TransactionList />
         </section>
       </div>
